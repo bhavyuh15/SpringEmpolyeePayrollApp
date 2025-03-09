a949e01cd764c314312886bc8dd4714839981a12
bhavyuh15 <bhavyadatey15@gmail.com>
2025-03-09 14:22:09 +0530

    [Bhavya] added UC2

--- EmployeePayrollApp/EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.EmployeePayrollApp;
+package com.bridgelabz.employeePayrollApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+
 }
